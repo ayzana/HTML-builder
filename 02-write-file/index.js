c4738f4addf01c8ee5682c52d17cfbae071f7024
ayzana <aizanka001@gmail.com>
2024-01-22 17:07:21 +0300

    fix: correct 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,11 +3,12 @@ const path = require('path');
 let writeName = fs.createWriteStream(path.join(__dirname, 'name.txt'));
 const readline = require('readline');
 let rl = readline.createInterface(process.stdin, process.stdout);
-rl.question('What is your name?', (name) => {
-  writeName.write(name);
-  console.log('Hello, ' + name);
+rl.question('Hello, enter text, please', () => {
   rl.on('line', (input) => {
-    if (input.toString() == 'exit') {
+    if (input.toString() !== 'exit') {
+      writeName.write(input);
+    } else {
+      console.log('You entered Exit, bye');
       rl.close();
     }
   });
