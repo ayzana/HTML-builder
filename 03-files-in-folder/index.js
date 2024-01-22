0a04c28891f0ba4007116c968062d89abe9c1f93
ayzana <aizanka001@gmail.com>
2024-01-22 18:02:23 +0300

    feat: add output files info in  03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,9 +4,10 @@ fs.readdir(path.join(__dirname, 'secret-folder'), (error, file) => {
   if (error) {
     console.log(error);
   } else {
-    console;
     file.forEach((el) => {
-      console.log(el);
+      fs.stat(`03-files-in-folder/secret-folder/${el}`, (err, stats) => {
+        console.log(`${el} - size: ${stats.size} birthtime:  ${stats.atime}`);
+      });
     });
   }
 });
