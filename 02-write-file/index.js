84fd87af5fb34fa2351d59dce4edbe065aeb4872
ayzana <aizanka001@gmail.com>
2024-01-24 09:35:15 +0300

    fix: correct 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 const path = require('path');
-let writeName = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+let writeN = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 const readline = require('readline');
 let rl = readline.createInterface(process.stdin, process.stdout);
-rl.question('Hello, enter text, please', () => {
+rl.question('Hello, enter text, please', (text) => {
+  writeN.write(text + '\n');
   rl.on('line', (input) => {
     if (input.toString() !== 'exit') {
-      writeName.write(input);
+      writeN.write(input + '\n');
     } else {
       console.log('You entered Exit, bye');
       rl.close();
