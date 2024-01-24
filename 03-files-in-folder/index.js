4f624e4fcf84de9a90f7606cc096879a22718e28
ayzana <aizanka001@gmail.com>
2024-01-24 10:25:31 +0300

    fix: correct 03

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,21 +1,30 @@
 const fs = require('fs');
 const path = require('path');
-fs.readdir(path.join(__dirname, 'secret-folder'), (error, file) => {
-  if (error) {
-    console.log(error);
-  } else {
-    file.forEach((el) => {
-      if (path.extname(el)) {
-        fs.stat(`03-files-in-folder/secret-folder/${el}`, (err, stats) => {
-          console.log(
-            `${el.slice(0, el.indexOf('.'))} - ${path.extname(el).slice(1)} - ${
-              stats.size / 1000
-            } kb`,
+fs.readdir(
+  path.join(__dirname, 'secret-folder'),
+  { withFileTypes: true },
+  (error, file) => {
+    if (error) {
+      console.log(error);
+    } else {
+      file.forEach((el) => {
+        if (el.isFile() === true) {
+          fs.stat(
+            `03-files-in-folder/secret-folder/${el.name}`,
+            (err, stats) => {
+              let name = el.name;
+              name = name.toString();
+              console.log(
+                `${name.slice(0, name.indexOf('.'))} - ${path
+                  .extname(name)
+                  .slice(1)} - ${stats.size / 1000} kb`,
+              );
+            },
           );
-        });
-      } else {
-        return;
-      }
-    });
-  }
-});
+        } else {
+          return;
+        }
+      });
+    }
+  },
+);
