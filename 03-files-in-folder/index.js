26c147311f76ee32e838d02ebc4f04592827b75b
ayzana <aizanka001@gmail.com>
2024-01-24 09:59:08 +0300

    fix: correct 03

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,9 +5,17 @@ fs.readdir(path.join(__dirname, 'secret-folder'), (error, file) => {
     console.log(error);
   } else {
     file.forEach((el) => {
-      fs.stat(`03-files-in-folder/secret-folder/${el}`, (err, stats) => {
-        console.log(`${el} - ${path.extname(el)} - ${stats.size / 1000} kb`);
-      });
+      if (path.extname(el)) {
+        fs.stat(`03-files-in-folder/secret-folder/${el}`, (err, stats) => {
+          console.log(
+            `${el.slice(0, el.indexOf('.'))} - ${path.extname(el).slice(1)} - ${
+              stats.size / 1000
+            } kb`,
+          );
+        });
+      } else {
+        return;
+      }
     });
   }
 });
